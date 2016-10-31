b2c03eb12d2d26b541916581561a0246d66c2b58
ZhenyingZhu <zz2283@columbia.edu>
2016-10-31 00:09:53 -0700

    Complete read UrlParser.py

--- python-example/UrlParser.py
+++ python-example/UrlParser.py
@@ -24,13 +24,17 @@ class HtmlParser:
     def parseUrls(self, content):
         # re.findall: return all match pattern from left to right
         # r or R prefix: valid escape sequences will not be converted
-        # \s: any non-whitespace char
+        # \s: any whitespace char
         # *: 0 or more repetition of prev
         # (?iLmsux): set flags like i means re.I for the comming string. here means href ignore case
-        # 
+        # "?: 0 or 1 double quote
+        # \'?: 0 or 1 single quote. need escape because the pattern start with single quote
+        # (...): the content in it can be retrive as group
+        # [^...]: indicate not match either char in the brace
         # re.I: ignore case
         links = re.findall(r'\s*(?i)href\s*=\s*"?\'?([^"\'>\s]*)', content, re.I)
 
+        links = [link for link in links if len(link) and not link.startswith('#')]
         return links
 
 
