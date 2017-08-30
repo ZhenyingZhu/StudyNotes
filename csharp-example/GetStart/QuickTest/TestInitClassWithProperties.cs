604c6f4d29a47ae177f4826b8092d318582b9292
Zhenying Zhu <zz2283@columbia.edu>
2017-08-29 20:11:18 -0700

    Add a small test of C#

---
+++ csharp-example/GetStart/QuickTest/TestInitClassWithProperties.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZhenyingTest
+{
+    abstract class Parent
+    {
+        public int Id { get; set; }
+    }
+
+    class Child : Parent
+    {
+        public string Name { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Child child = new Child()
+            {
+                Id = 1,
+                Name = "child"
+            };
+
+            Console.WriteLine("See if it works: " + child.Name + child.Id);
+            
+            Console.ReadLine();
+        }
+
+    }
+}
