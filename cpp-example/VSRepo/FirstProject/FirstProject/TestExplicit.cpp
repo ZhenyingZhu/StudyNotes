cff289b176d2aa2996b6622cceba2275056fe0ef
Zhenying Zhu <zz2283@columbia.edu>
2017-10-06 21:29:31 -0700

    Test explicit and constructor

---
+++ cpp-example/VSRepo/FirstProject/FirstProject/TestExplicit.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+namespace TestExplicit {
+class MyClassProperty {
+public:
+	MyClassProperty() : num(0) {}
+
+	MyClassProperty(int n) : num(n) {}
+
+	~MyClassProperty() {
+		cout << "Destroying MyClassProperty " << num << endl;
+	}
+
+private:
+	int num;
+};
+
+class MyClass {
+public:
+	// converting constructor
+	MyClass(int n) : num(n), p(new string()), r1(1), r2(2) {
+	}
+
+	MyClass(int n, const string &s) : num(n), p(new string(s)), r1(1), r2(2) {
+	}
+
+	MyClass(const MyClass &x) {
+		num = x.num;
+		p = new string(*(x.p));
+		r1 = MyClassProperty(1);
+		r2 = MyClassProperty(2);
+	}
+
+	MyClass& operator=(const MyClass &x) {
+		num = x.num;
+		p = new string(*(x.p));
+		r1 = MyClassProperty(1);
+		r2 = MyClassProperty(2);
+		return *this;
+	}
+
+	int getNum() const {
+		return num;
+	}
+
+	void setNum(int n) {
+		num = n;
+	}
+
+	~MyClass() {
+		delete p;
+		cout << "delete p" << endl;
+	}
+
+private:
+	int num;
+	string* p;
+	MyClassProperty r2;
+	MyClassProperty r1;
+};
+
+int TestExplicitmain() {
+	int i = 1;
+	string s("Hello World!");
+
+	MyClass mc1(i, s);
+
+	cout << mc1.getNum() << endl;
+
+	return 0;
+}
+}
