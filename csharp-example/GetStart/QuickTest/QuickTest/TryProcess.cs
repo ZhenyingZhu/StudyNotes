77b521cde20a41b1e3081d27382304098f950796
Zhenying Zhu <zz2283@columbia.edu>
2017-11-20 14:30:08 -0800

    Test C# process.

---
+++ csharp-example/GetStart/QuickTest/QuickTest/TryProcess.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickTest
+{
+    class TryProcess
+    {
+        public static void TestMain()
+        {
+            string path = "C:\\windows\\system32\\notepad.exe";
+            var startInfo = new ProcessStartInfo(path)
+            {
+                Arguments =
+                    "test.txt",
+                UseShellExecute = false
+            };
+
+            Process process = Process.Start(startInfo);
+            if (process != null)
+            {
+                process.WaitForExit(1000);
+                if (process.HasExited)
+                {
+                    Console.WriteLine(process.ExitCode);
+                }
+                else
+                {
+                    Console.WriteLine("Not exit.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Process is null. Something Wrong.");
+            }
+        }
+    }
+}
