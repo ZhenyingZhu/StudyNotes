2919d3e517a3896b48aa417c6ba539619c38682c
Zhenying Zhu <zz2283@columbia.edu>
2017-12-09 23:22:38 -0800

    update sql example.

--- mysql-example/TaggedFS/SearchFilesWithATag.sql
+++ mysql-example/TaggedFS/SearchFilesWithATag.sql
@@ -4,6 +4,6 @@ SELECT file.id, file.name
 FROM file, filetag, tag
 WHERE filetag.file=file.id
 AND filetag.tag=tag.id
-AND (tag.name="credit")
+AND (tag.name="diploma")
 GROUP BY file.id
 ORDER BY file.id
