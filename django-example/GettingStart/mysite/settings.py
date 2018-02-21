0512c1304e99a584047f8cafa7e10f71aac71f22
ZhenyingZhu <zz2283@columbia.edu>
2018-02-20 23:57:50 -0800

    Trying to test with Django static folder. Doesn't work well.

--- django-example/GettingStart/mysite/settings.py
+++ django-example/GettingStart/mysite/settings.py
@@ -101,4 +101,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'todolist/src/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'todolist/src/'),
+)
