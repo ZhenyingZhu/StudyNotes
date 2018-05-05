3c8e65777bf4de898be5c03e595b20e50a203c43
Zhenying Zhu <zz2283@columbia.edu>
2018-05-04 19:23:22 -0700

    Migrate to Windows.

--- django-example/GettingStart/mysite/urls.py
+++ django-example/GettingStart/mysite/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin.site.urls),
     url(r'', include('todolist.urls')),
 ]
