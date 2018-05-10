4b5a00cd92e2ed380ec7c740ad2f2e6a5ce109d5
Zhenying Zhu <zz2283@columbia.edu>
2018-05-10 00:04:56 -0700

    Add a powershell script that test multi-thread.

---
+++ powershell-example/TestMultiThread.ps1
@@ -0,0 +1,24 @@
+﻿$ScriptBlock =
+{
+    param ($num)
+
+    Write-Host "start $num"
+    Start-Sleep 5
+    Write-Host "end $num"
+}
+
+$threadNum = 3
+
+$currentThreadCnt = 0
+for ($i = 1; $i -le 10; $i++)
+{
+    Start-Job $ScriptBlock -ArgumentList $i
+    $currentThreadCnt++
+    if ($currentThreadCnt -eq $threadNum)
+    {
+        $currentThreadCnt = 0
+        Get-Job | Wait-Job
+        Get-Job | Receive-Job
+    }
+}
+
