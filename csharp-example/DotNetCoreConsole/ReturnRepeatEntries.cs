d2f4147e086c7dd462025b932b7141c7067dcc69
Zhenying Zhu <zz2283@columbia.edu>
2018-05-15 19:29:50 -0700

    Add a test for Dotnet.

---
+++ csharp-example/DotNetCoreConsole/ReturnRepeatEntries.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ReturnRepeatEntries
+{
+    public class MyClass
+    {
+        public int num
+        {
+            set;
+            get;
+        }
+
+        public bool shouldBeSelected
+        {
+            set;
+            get;
+        }
+    }
+
+    public class TestMain
+    {
+        public static void testMain()
+        {
+            List<MyClass> myList = new List<MyClass>();
+            for (int i = 0; i < 3; i++)
+            {
+                MyClass mci = new MyClass
+                {
+                    num = i,
+                    shouldBeSelected = true
+                };
+                myList.Add(mci);
+            }
+
+            MyClass mc = new MyClass
+            {
+                num = 2,
+                shouldBeSelected = true
+            };
+            myList.Add(mc);
+
+            List<int> res = myList.Where(x => x.shouldBeSelected).Select(x => x.num).ToList();
+            System.Console.WriteLine("[" + String.Join(", ", res) + "]");
+        }
+    }
+}
