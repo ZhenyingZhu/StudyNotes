6a99db31aa11e351c464c26666c554c09a5ab6d7
Zhenying Zhu <zz2283@columbia.edu>
2018-12-30 18:01:56 -0800

    Learning tampermonkey.

---
+++ tampermonkey-example/SteamWishlistParser.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Steam Wishlist Parser
+// @namespace    https://github.com/shenjihehe/steamdiscount-/blob/master/steamwhlistspider.py
+// @version      0.1
+// @description  The original script is poorly written. Trying to convert it into a tampermonkey script. Note Steam already support it.
+// @author       baodongsunshenjihehe
+// @match        https://store.steampowered.com/wishlist/
+// @grant        None
+// ==/UserScript==
+
+debugger;
+
+(function() {
+    'use strict';
+
+})();
