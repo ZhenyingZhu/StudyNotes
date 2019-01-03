3b74f998e3a7a7ff0a42bfeb327d1bf3b1cdb6ba
Zhenying Zhu <zz2283@columbia.edu>
2019-01-02 22:58:07 -0800

    Learning tampermonkey.

--- tampermonkey-example/SteamWishlistParser.js
+++ tampermonkey-example/SteamWishlistParser.js
@@ -3,8 +3,8 @@
 // @namespace    https://github.com/shenjihehe/steamdiscount-/blob/master/steamwhlistspider.py
 // @version      0.1
 // @description  The original script is poorly written. Trying to convert it into a tampermonkey script. Note Steam already support it.
-// @author       baodongsunshenjihehe
-// @match        https://store.steampowered.com/wishlist/
+// @author       shenjihehe
+// @match        https://store.steampowered.com/wishlist/*
 // @grant        None
 // ==/UserScript==
 
@@ -12,7 +12,17 @@ debugger;
 
 (function() {
     'use strict';
-
+})();
+// It doesn't work. The issue is the the page load after the script executes.
+window.onload(function exec() {
+    var textMessage = "",
+        appId;
     var info = $J(".wishlist_row:has(.discount_pct)");
 
+    info.filter(function() {
+        appId = $(this).getAttribute('data-app-id');
+        textMessage += appId;
+    });
+
+    alert(textMessage);
 })();
