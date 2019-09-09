652650e4d89deb910ecdb40f8ee450c8e938987e
Zhenying Zhu <zz2283@columbia.edu>
2019-09-09 11:23:08 -0700

    Learning how to use timespan in C#.

---
+++ csharp-example/TestTimeSpan/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TestTimeSpan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeSpan span = TimeSpan.FromMinutes(1);
+
+            Console.WriteLine("Time span {0}", span.TotalSeconds);
+        }
+    }
+}
