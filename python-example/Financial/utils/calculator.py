5629b8188b55ade2252c7bfd58aeb8f83b51fae5
Zhenying Zhu <zz2283@columbia.edu>
2019-11-15 02:08:52 -0800

    Complete video 2.

--- python-example/Financial/utils/calculator.py
+++ python-example/Financial/utils/calculator.py
@@ -1,3 +1,4 @@
+import math
 
 def compound(base, rate, duration):
     coumpounding = base
@@ -20,6 +21,13 @@ def annuity(base, payment, rate, duration):
         saving += payment # add after interest because often next payment is after a period since base put in.
     return saving
 
+def annuityBeginOfYear(base, payment, rate, duration):
+    saving = base
+    for _ in range(duration):
+        saving += payment
+        saving *= 1.0 + rate / 100.0
+    return saving
+
 def backward_annuity(target, base, rate, duration):
     # target = b*(1+r)^d + p*sum((1+r)^x, x=0..d-1) = b*(1+r)^d + p*((1+r)^d-1)/r
 
@@ -40,6 +48,24 @@ def backward_annuity(target, base, rate, duration):
     # payment
     return val3 / val4
 
+def nominalRateOfReturn(start_value, end_value, duration):
+    # startValue*(1+r)^d=endValue
+    val = end_value / start_value
+    rate = pow(val, 1.0 / duration) - 1
+    return rate * 100.0
 
+def realRateOfReturn(start_value, end_value, inflation, duration):
+    nominal_rate = nominalRateOfReturn(start_value, end_value, duration) / 100.0
+    # 1+rr = (1+nr)/(1+in)
+    realRate = (1 + nominal_rate) / (1 + inflation / 100.0) - 1
+    return realRate * 100.0
 
-
+def compoundDuration(base, rate, payment, target):
+    duration = 0
+    val = base
+    while (val < target):
+        val *= 1 + rate / 100.0
+        val += payment
+        duration += 1
+    
+    return duration
