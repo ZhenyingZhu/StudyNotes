c45793741920e5770c196b0035cb0c595aebfdf1
Zhenying Zhu <zz2283@columbia.edu>
2019-11-17 21:23:29 -0800

    Trying to create a new entity.

--- dotnet-example/WebApplicationMVC/WebApplicationMVC/Data/ApplicationDbContext.cs
+++ dotnet-example/WebApplicationMVC/WebApplicationMVC/Data/ApplicationDbContext.cs
@@ -18,13 +18,15 @@ namespace WebApplicationMVC.Data
 
         public DbSet<AppTestChildModel> AppTestChildModels { get; set; }
 
+        public DbSet<UserSpecificItemModel> UserSpecificItemModel { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
             modelBuilder.Entity<AppTestModel>().Property(t => t.AppTestInput).HasMaxLength(20);
 
-            modelBuilder.Entity<AppTestModel>().HasData(new Models.AppTestModel() { Id = 10, AppTestInput = "Seeding Test1" });
+            modelBuilder.Entity<AppTestModel>().HasData(new AppTestModel() { Id = 10, AppTestInput = "Seeding Test1" });
         }
     }
 }
---
+++ dotnet-example/WebApplicationMVC/WebApplicationMVC/Data/Migrations/20191118050943_UserSpecificItem.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplicationMVC.Data.Migrations
+{
+    public partial class UserSpecificItem : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserSpecificItemModel",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserSpecificItemModel", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserSpecificItemModel");
+        }
+    }
+}
