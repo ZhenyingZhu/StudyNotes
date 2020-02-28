257d6f1784d19c92030c783324853eb96039c37b
Zhenying Zhu <zz2283@columbia.edu>
2020-02-27 23:58:50 -0800

    Worse course I've even attend.

--- dotnet-example/ReactCustomWebpack/ClientApp/Components/Common/FullPage.js
+++ dotnet-example/ReactCustomWebpack/ClientApp/Components/Common/FullPage.js
@@ -1,17 +1,32 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
-import {BrowserRouter as Router} from 'react-router-dom';
-import {Route, Switch} from 'react-router-dom';
 
-import Routes from '../../Routes'
+import CodeCampMenu from './CodeCampMenu';
+import PageTop from './PageTop';
+import Footer from './Footer';
+import Routes from "../../Routes";
+
 
 class FullPage extends Component {
+
+    constructor(props){
+        super(props);
+        this.handler = this.handler.bind(this);
+    }
+
+    handler(val) {
+        this.props.action();
+    }
+
+
     render() {
         return (
             <div>
-                <h2>Page Top</h2>
-                {/* zhenying: need use curly braces to wrap comment. */}
-                <Routes />
+                <PageTop>
+                    <CodeCampMenu />
+                </PageTop>
+                <Routes  action={this.handler}  />
+                <Footer />
             </div>
         );
     }
