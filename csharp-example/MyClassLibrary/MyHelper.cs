da720c5a0467feff4ea54b8c2e103633c555a24c
Zhenying Zhu <zz2283@columbia.edu>
2020-09-17 22:44:03 -0700

    Trying to add a project reference.

---
+++ csharp-example/MyClassLibrary/MyHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyClassLibrary
+{
+    public class MyHelper
+    {
+        public static void PrintHelloMessage()
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
--- csharp-example/MyConsoleAppTest/Program.cs
+++ csharp-example/MyConsoleAppTest/Program.cs
@@ -4,13 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using MyClassLibrary;
+
 namespace MyConsoleAppTest
 {
     class Program
     {
         static void Main(string[] args)
         {
-
+            MyHelper.PrintHelloMessage();
         }
     }
 }
