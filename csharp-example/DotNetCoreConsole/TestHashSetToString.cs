b6114ba9ed81e5d2b0832b755a9245d68deb8af4
Zhenying Zhu <zz2283@columbia.edu>
2020-10-18 18:55:09 -0700

    Test how HashSet is print.

---
+++ csharp-example/DotNetCoreConsole/TestHashSetToString.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace DotNetCoreConsole
+{
+    public class TestHashSetToString
+    {
+        public static void testMain()
+        {
+            HashSet<int> values = new HashSet<int>{1, 2, 3};
+            Console.WriteLine(string.Join(",", values));
+        }
+    }
+}
