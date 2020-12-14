905c4fc916971918513681ab84377b3dcaeb1add
Zhenying Zhu <zz2283@columbia.edu>
2020-12-13 17:07:36 -0800

    Learning MSBuild task.

--- csharp-example/DotNetCoreConsole/SimpleTask.cs
+++ csharp-example/DotNetCoreConsole/SimpleTask.cs
@@ -2,10 +2,11 @@ using System;
 using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
 
-namespace MyTasks
+namespace DotNetCoreConsole
 {
     public class SimpleTask : Task
     {
+        // Seems like MSBuild work differently then the dotnet core projects.
         public override bool Execute()
         {
             return true;
