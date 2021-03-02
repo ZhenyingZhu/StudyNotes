1defae184164f7b0c613012152cc825adebe3072
Zhenying Zhu <zz2283@columbia.edu>
2021-03-01 21:39:40 -0800

    add a note about how ps set parm.

--- powershell-example/ZipFolders.ps1
+++ powershell-example/ZipFolders.ps1
@@ -8,6 +8,17 @@
 
 # https://stackoverflow.com/questions/18847145/loop-through-files-in-a-directory-using-powershell
 
+param
+(
+    [Parameter(Mandatory=$true, HelpMessage='The path')]
+    [ValidateNotNullOrEmpty()]
+    $FoldersToZip,
+
+    [Parameter(Mandatory=$true, HelpMessage='The path')]
+    [ValidateNotNullOrEmpty()]
+    $DestinationPath
+)
+
 Get-ChildItem -Path "FoldersToZip" -Recurse |
 ForEach-Object {
     $itemName = $_.FullName
@@ -17,7 +28,6 @@ ForEach-Object {
         Write-Host "zipping $itemName..."
         Compress-Archive -Force -Path $itemName -DestinationPath "$itemName.zip"
     }
-    
 }
 
 # Powershell.exe -command "& { Compress-Archive -Force -path "xyz" -DestinationPath xyz.zip }"
