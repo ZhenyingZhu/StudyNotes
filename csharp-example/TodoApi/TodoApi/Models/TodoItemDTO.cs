5ecd143e2970886ac46b3b9937da452872fc5e97
Zhenying Zhu <zz2283@columbia.edu>
2022-04-03 16:55:06 -0700

    Learning .NET Core

---
+++ csharp-example/TodoApi/TodoApi/Models/TodoItemDTO.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TodoApi.Models
+{
+    [ModelMetadataType(typeof(TodoItem))]
+    public class TodoItemDTO
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
