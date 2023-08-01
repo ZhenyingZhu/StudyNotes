11d38c6ab6cec0b2ad338be3c616054014b8d712
Zhenying Zhu <zz2283@columbia.edu>
2023-07-31 23:12:24 -0700

    Trying to run the code but failed.

--- csharp-example/AzureManagementTest/Program.cs
+++ csharp-example/AzureManagementTest/Program.cs
@@ -1,93 +1,95 @@
-﻿// See https://aka.ms/new-console-template for more information
-using Azure.Identity;
+﻿using Azure.Identity;
 using Azure.ResourceManager;
 using Azure.ResourceManager.Resources;
 using Azure.ResourceManager.Compute;
 
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
+// Follow https://github.com/Azure/azure-sdk-for-net/blob/main/sdk/identity/Azure.Identity/README.md#authenticate-via-visual-studio-code
+// Use Azure Account extension to sign in.
 ArmClient client = new ArmClient(new DefaultAzureCredential());
-
 SubscriptionResource subscription = client.GetDefaultSubscription();
 
-// VirtualMachineCollection vms
-string subscriptionId = "YOUR_SUBSCRIPTION_ID";
-string clientId = "YOUR_CLIENT_ID";
-            string clientSecret = "YOUR_CLIENT_SECRET";
-            string tenantId = "YOUR_TENANT_ID";
-            string resourceGroupName = "YOUR_RESOURCE_GROUP_NAME";
-            string vmName = "YOUR_VM_NAME";
-            string location = "YOUR_VM_LOCATION"; // e.g., "eastus"
+var resourceGroup = subscription.GetResourceGroup("WinServer2022VM");
 
-            // Authenticate using a service principal
-            var serviceClientCredentials = ApplicationTokenProvider.LoginSilentAsync(
-                tenantId, clientId, clientSecret).Result;
+// Below code written by chatgpt
+// // Create the resource group
+// var resourceGroup = new ResourceGroup
+// {
+//     Location = location
+// };
 
-            // Create a Resource Management client
-            var resourceManagementClient = new ResourceManagementClient(serviceClientCredentials)
-            {
-                SubscriptionId = subscriptionId
-            };
+// // VirtualMachineCollection vms
+// string subscriptionId = "YOUR_SUBSCRIPTION_ID";
+// string clientId = "YOUR_CLIENT_ID";
+// string clientSecret = "YOUR_CLIENT_SECRET";
+// string tenantId = "YOUR_TENANT_ID";
+// string resourceGroupName = "YOUR_RESOURCE_GROUP_NAME";
+// string vmName = "YOUR_VM_NAME";
+// string location = "YOUR_VM_LOCATION"; // e.g., "eastus"
 
-            // Create the resource group
-            var resourceGroup = new ResourceGroup
-            {
-                Location = location
-            };
+// // Authenticate using a service principal
+// var serviceClientCredentials = ApplicationTokenProvider.LoginSilentAsync(tenantId, clientId, clientSecret).Result;
 
-            resourceManagementClient.ResourceGroups.CreateOrUpdate(resourceGroupName, resourceGroup);
+// // Create a Resource Management client
+// var resourceManagementClient = new ResourceManagementClient(serviceClientCredentials)
+// {
+//     SubscriptionId = subscriptionId
+// };
 
-            // Create the VM
-            var vmClient = new ComputeManagementClient(serviceClientCredentials)
-            {
-                SubscriptionId = subscriptionId
-            };
-            var vmParams = new VirtualMachine
-            {
-                Location = location,
-                OsProfile = new OSProfile
-                {
-                    ComputerName = vmName,
-                    AdminUsername = "YOUR_ADMIN_USERNAME", // Replace with your desired admin username
-                    AdminPassword = "YOUR_ADMIN_PASSWORD"  // Replace with your desired admin password
-                },
-                HardwareProfile = new HardwareProfile
-                {
-                    VmSize = "Standard_D2s_v3" // Replace with the desired VM size
-                },
-                StorageProfile = new StorageProfile
-                {
-                    ImageReference = new ImageReference
-                    {
-                        Publisher = "MicrosoftWindowsServer",
-                        Offer = "WindowsServer",
-                        Sku = "2022-datacenter",
-                        Version = "latest"
-                    },
-                    OsDisk = new OSDisk
-                    {
-                        CreateOption = DiskCreateOptionTypes.FromImage,
-                        ManagedDisk = new ManagedDiskParameters
-                        {
-                            StorageAccountType = StorageAccountTypes.StandardLRS
-                        }
-                    }
-                },
-                NetworkProfile = new NetworkProfile
-                {
-                    NetworkInterfaces = new[]
-                    {
-                        new NetworkInterfaceReference
-                        {
-                            Id = "/subscriptions/YOUR_SUBSCRIPTION_ID/resourceGroups/YOUR_RESOURCE_GROUP/providers/Microsoft.Network/networkInterfaces/YOUR_NETWORK_INTERFACE_NAME"
-                            // Replace with the actual network interface ID
-                        }
-                    }
-                }
-            };
-            vmClient.VirtualMachines.CreateOrUpdate(resourceGroupName, vmName, vmParams);
+// resourceManagementClient.ResourceGroups.CreateOrUpdate(resourceGroupName, resourceGroup);
 
-            Console.WriteLine("Virtual Machine created successfully!");
+// // Create the VM
+// var vmClient = new ComputeManagementClient(serviceClientCredentials)
+// {
+//     SubscriptionId = subscriptionId
+// };
 
+// var vmParams = new VirtualMachine
+// {
+//     Location = location,
+//     OsProfile = new OSProfile
+//     {
+//         ComputerName = vmName,
+//         AdminUsername = "YOUR_ADMIN_USERNAME", // Replace with your desired admin username
+//         AdminPassword = "YOUR_ADMIN_PASSWORD"  // Replace with your desired admin password
+//     },
+//     HardwareProfile = new HardwareProfile
+//     {
+//         VmSize = "Standard_D2s_v3" // Replace with the desired VM size
+//     },
+//     StorageProfile = new StorageProfile
+//     {
+//         ImageReference = new ImageReference
+//         {
+//             Publisher = "MicrosoftWindowsServer",
+//             Offer = "WindowsServer",
+//             Sku = "2022-datacenter",
+//             Version = "latest"
+//         },
+//         OsDisk = new OSDisk
+//         {
+//             CreateOption = DiskCreateOptionTypes.FromImage,
+//             ManagedDisk = new ManagedDiskParameters
+//             {
+//                 StorageAccountType = StorageAccountTypes.StandardLRS
+//             }
+//         }
+//     },
+//     NetworkProfile = new NetworkProfile
+//     {
+//         NetworkInterfaces = new[]
+//         {
+//                         new NetworkInterfaceReference
+//                         {
+//                             Id = "/subscriptions/YOUR_SUBSCRIPTION_ID/resourceGroups/YOUR_RESOURCE_GROUP/providers/Microsoft.Network/networkInterfaces/YOUR_NETWORK_INTERFACE_NAME"
+//                             // Replace with the actual network interface ID
+//                         }
+//                     }
+//     }
+// };
 
-ResourceGroupResource resourceGroup = subscription.GetResourceGroup("WinServer2022VM");
+// vmClient.VirtualMachines.CreateOrUpdate(resourceGroupName, vmName, vmParams);
+
+// Console.WriteLine("Virtual Machine created successfully!");
