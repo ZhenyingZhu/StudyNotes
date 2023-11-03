523ffde8889dae56df3f826f315d5526f2eb6bea
Zhenying Zhu <zz2283@columbia.edu>
2023-11-02 23:03:54 -0700

    Pass in the tenant id

--- csharp-example/AzureManagementTest/Program.cs
+++ csharp-example/AzureManagementTest/Program.cs
@@ -9,7 +9,10 @@ Console.WriteLine("Hello, World!");
 // Follow https://github.com/Azure/azure-sdk-for-net/blob/main/sdk/identity/Azure.Identity/README.md#authenticate-via-visual-studio-code
 // Use Azure Account extension to sign in.
 // Seems like need to set env vars.
-ArmClient client = new ArmClient(new DefaultAzureCredential(true));
+ArmClient client = new ArmClient(new VisualStudioCredential(new VisualStudioCredentialOptions()
+{
+    TenantId = "c74a24d8-2986-4739-aec1-36b4c9934ed3"
+}));
 
 SubscriptionCollection subscriptions = client.GetSubscriptions();
 SubscriptionResource sub = subscriptions.FirstOrDefault();
