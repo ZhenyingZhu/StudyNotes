9bf2bfdb0eee7c94710f82ffdceebf2fd5410331
Zhenying Zhu <zz2283@columbia.edu>
2024-02-09 12:09:55 -0800

    Add a code to replace SF version

--- csharp-example/DotNetCoreConsole/Program.cs
+++ csharp-example/DotNetCoreConsole/Program.cs
@@ -9,7 +9,7 @@ namespace DotNetCoreConsole
     {
         static void Main(string[] args)
         {
-            TestTopologySort.TestMain();
+            ServiceFabricVersionUpgrade.TestMain();
         }
     }
 }
