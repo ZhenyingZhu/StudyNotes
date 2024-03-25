16cc9405a02a96e93a5c9447e9c05e5f67e68192
Zhenying Zhu <zz2283@columbia.edu>
2024-03-24 22:19:41 -0700

    Add an ASP.NET project

---
+++ dotnet-example/AspNetApplication/TestWebApplication/Areas/HelpPage/ModelDescriptions/ComplexTypeModelDescription.cs
@@ -0,0 +1,14 @@
+using System.Collections.ObjectModel;
+
+namespace TestWebApplication.Areas.HelpPage.ModelDescriptions
+{
+    public class ComplexTypeModelDescription : ModelDescription
+    {
+        public ComplexTypeModelDescription()
+        {
+            Properties = new Collection<ParameterDescription>();
+        }
+
+        public Collection<ParameterDescription> Properties { get; private set; }
+    }
+}
