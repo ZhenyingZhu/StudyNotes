6c9e49afae3d7589fda8d00f04dde80b77ec96fe
ZhenyingZhu <zz2283@columbia.edu>
2025-01-24 17:53:22 -0800

    Add code for calling powershell

--- csharp-example/DotNetCoreExample/DotNetCoreConsole/Program.cs
+++ csharp-example/DotNetCoreExample/DotNetCoreConsole/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Management.Automation;
 
 namespace DotNetCoreConsole
 {
@@ -9,7 +10,23 @@ namespace DotNetCoreConsole
     {
         static void Main(string[] args)
         {
-            ServiceFabricVersionUpgrade.TestMain();
+                using (PowerShell powerShell = PowerShell.Create())
+                {
+                    // Add a cmdlet to the PowerShell pipeline
+                    powerShell.AddCommand("Get-Process");
+
+                    // Optionally, add parameters if needed
+                    // powerShell.AddParameter("Name", "notepad");
+
+                    // Execute the command
+                    var results = powerShell.Invoke();
+
+                    // Process and display the results
+                    foreach (var result in results)
+                    {
+                        Console.WriteLine(result);
+                    }
+                }
         }
     }
 }
