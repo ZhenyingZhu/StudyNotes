e7042513d450291997ddc21fc99f1dfd48505e77
Zhenying Zhu <zhenying.zhu91@gmail.com>
2025-07-29 19:05:47 -0700

    Trying to see why AfterBuild is override

---
+++ dotnet-example/SDKStyle/Program.cs
@@ -0,0 +1,28 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Program.cs" company="Microsoft">
+//     Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+// <author>Jon Rowlett</author>
+//-----------------------------------------------------------------------
+
+namespace SDKStyle
+{
+    using System;
+
+    /// <summary>
+    /// The main entry point of the program.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// The Main routine of the program.
+        /// </summary>
+        /// <param name="args">the arguments to the program.</param>
+        /// <returns>the pass or fail status.</returns>
+        public static int Main(string[] args)
+        {
+            Console.WriteLine("This is a SDK style program.");
+            return 0;
+        }
+    }
+}
