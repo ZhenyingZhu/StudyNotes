e961e50eaef8d42b8ddc0ec57f9b0cafdde098a3
ZhenyingZhu <zz2283@columbia.edu>
2026-08-23 16:00:09 -0700

    Python env setup correctly

--- LLM-example/identify_bird.py
+++ LLM-example/identify_bird.py
@@ -1,6 +1,7 @@
 from ddgs import DDGS
 from fastcore.all import *
+import time, json
 
 def search_images(keywords, max_images=200):
     return L(DDGS().images(keywords, max_results=max_images)).itemgot('image')
-import time, json
+
