441d688747832d1d9520965a43f5155e8e7e2a96
ZhenyingZhu <zz2283@columbia.edu>
2026-08-25 23:28:53 -0700

    Learn Kaggle

--- LLM-example/identify_bird.py
+++ LLM-example/identify_bird.py
@@ -1,7 +1,16 @@
 from ddgs import DDGS
 from fastcore.all import *
+from fastdownload import download_url
+from fastai.vision.all import *
 import time, json
 
 def search_images(keywords, max_images=200):
     return L(DDGS().images(keywords, max_results=max_images)).itemgot('image')
 
+urls = search_images('bird photos', max_images=1)
+
+dest = 'bird.jpg'
+download_url(urls[0], dest, show_progress=False)
+
+im = Image.open(dest)
+im.to_thumb(256,256)
