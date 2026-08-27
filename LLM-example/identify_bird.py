8dfbf24cf06fdfd4674a7701cc2cd82e3c13965c
ZhenyingZhu <zz2283@columbia.edu>
2026-08-26 21:57:52 -0700

    Learn python show. Seems like it needs jupter notebook

--- LLM-example/identify_bird.py
+++ LLM-example/identify_bird.py
@@ -7,16 +7,16 @@ import time, json
 def search_images(keywords, max_images=200):
     return L(DDGS().images(keywords, max_results=max_images)).itemgot('image')
 
-urls = search_images('bird photos', max_images=1)
+# urls = search_images('bird photos', max_images=1)
 
-dest = 'bird.jpg'
-download_url(urls[0], dest, show_progress=False)
+# dest = 'bird.jpg'
+# download_url(urls[0], dest, show_progress=False)
 
-im = Image.open(dest)
-im.to_thumb(256,256)
+# im = Image.open(dest)
+# im.to_thumb(256,256)
 
-download_url(search_images('forest photos', max_images=1)[0], 'forest.jpg', show_progress=False)
-Image.open('forest.jpg').to_thumb(256,256)
+# download_url(search_images('forest photos', max_images=1)[0], 'forest.jpg', show_progress=False)
+# Image.open('forest.jpg').to_thumb(256,256)
 
 searches = 'forest','bird'
 path = Path('bird_or_not')
@@ -27,3 +27,17 @@ for o in searches:
     download_images(dest, urls=search_images(f'{o} photo'))
     time.sleep(5)
     resize_images(path/o, max_size=400, dest=path/o)
+
+failed = verify_images(get_image_files(path))
+failed.map(Path.unlink)
+len(failed)
+
+dls = DataBlock(
+    blocks=(ImageBlock, CategoryBlock), 
+    get_items=get_image_files, 
+    splitter=RandomSplitter(valid_pct=0.2, seed=42),
+    get_y=parent_label,
+    item_tfms=[Resize(192, method='squish')]
+).dataloaders(path, bs=32)
+
+dls.show_batch(max_n=6)
